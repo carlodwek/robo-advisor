a2fd2dfc4c0fa996787ab9657831f7ba81d76a4a
carlodwek <63809747+carlodwek@users.noreply.github.com>
2021-03-03 17:32:30 -0500

    update

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -16,3 +16,14 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+import requests
+import json
+import pandas
+import dotenv
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=NU0BYWLORVQMXJ1A"
+response = requests.get(request_url)
+print(type(response))
+print(response.status_code)
+print(response.text)
